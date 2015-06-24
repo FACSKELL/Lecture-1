d556f4516ad364090da6370c6ce2d72b605538c4
rjmk <rafekennedy@gmail.com>
2015-06-24 16:28:34 +0100

    solution to next bit

--- lecture-1.hs
+++ lecture-1.hs
@@ -25,3 +25,11 @@ validate :: Integer -> Bool
 validate n
   | (sumDigits . doubleEveryOther . toDigits) n `mod` 10 == 0 = True
   | otherwise                                                 = False
+
+type Peg = String
+type Move = (Peg, Peg)
+
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _                                                   = [ ]
+hanoi 1 a b c                                              = [(a, b)]
+hanoi n a b c = hanoi (n - 1) a c b ++ [(a, b)] ++ hanoi (n-1) c b a
